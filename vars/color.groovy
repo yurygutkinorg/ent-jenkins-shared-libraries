91827182389d302447174e9f11c0a60ff02f4a02
Patryk Przekwas <pkprzekwas@gmail.com>
2019-02-20 17:12:15 +0100

    Test for generate slave label

--- vars/color.groovy
+++ vars/color.groovy
@@ -1,14 +1,13 @@
-
 // color codes: http://pueblo.sourceforge.net/doc/manual/ansi_color_codes.html
-def call(String text, color='green'){
-  def colors = [ red : "41", green : "42", yellow : "43",
-  	blue : "44", magenta : "45", cyan : "46", white : "47" ]
+def call(String text, String color = 'green') {
+    Map colors = [red: "41", green: "42", yellow: "43", blue: "44", magenta: "45", cyan: "46", white: "47"]
+    def selected = colors[color.toLowerCase()]
 
-	selected = colors[color.toLowerCase()]
-	if (selected == null) {
-			selected = 48
-	}
-	wrap([$class: 'AnsiColorBuildWrapper']) {
-		echo "\u001B[${selected}m${text}\u001B[0m"
-	}
+    if (selected == null) {
+        selected = "48"
+    }
+
+    wrap([$class: 'AnsiColorBuildWrapper']) {
+        echo "\u001B[${selected}m${text}\u001B[0m"
+    }
 }
