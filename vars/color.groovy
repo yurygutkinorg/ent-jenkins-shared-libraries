2a08541fcb8287ecd0703615a677540759eea63d
Patryk Przekwas <pkprzekwas@gmail.com>
2019-02-20 17:00:30 +0100

    Coloring function

---
+++ vars/color.groovy
@@ -0,0 +1,12 @@
+
+// color codes: http://pueblo.sourceforge.net/doc/manual/ansi_color_codes.html
+def call(String text, color){
+  def colors = [ red : "41", green : "42", yellow : "43",
+  	blue : "44", magenta : "45", cyan : "46", white : "47" ]
+
+	selected = colors[color.toLowerCase()]
+
+	wrap([$class: 'AnsiColorBuildWrapper']) {
+		echo "\u001B[${selected}m ${text}\u001B[0m"
+	}
+}
