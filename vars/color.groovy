41585994933b6c4b3cb9b5133a3cd2c0bcbc1762
Patryk Przekwas <pkprzekwas@gmail.com>
2019-02-20 17:03:34 +0100

    Test for generate slave label (default value)

--- vars/color.groovy
+++ vars/color.groovy
@@ -1,6 +1,6 @@
 
 // color codes: http://pueblo.sourceforge.net/doc/manual/ansi_color_codes.html
-def call(String text, color){
+def call(String text, color='green'){
   def colors = [ red : "41", green : "42", yellow : "43",
   	blue : "44", magenta : "45", cyan : "46", white : "47" ]
 
