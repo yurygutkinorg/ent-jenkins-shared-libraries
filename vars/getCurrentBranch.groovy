9d86f4fe7d5619eb9c679a911977681ada4b9a5a
Patryk Przekwas <pkprzekwas@gmail.com>
2019-02-27 13:27:29 +0100

    Fix for multibranch pipeline

--- vars/getCurrentBranch.groovy
+++ vars/getCurrentBranch.groovy
@@ -2,5 +2,8 @@ def call() {
     if (env.GIT_BRANCH) {
         return env.GIT_BRANCH
     }
-    return scm.branches[0].name.drop(2)
+
+    branchFull = scm.branches[0].name.drop(2)
+    splitBranchFull = branchFull.split('/')
+    return splitBranchFull.last()
 }
