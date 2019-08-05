bec50678b0055958e6aec8b579c490514dac6637
pprzekwas <pprzekwas@guardanthealth.com>
2019-08-05 12:02:17 +0200

    Ent 10537 fix (#25)

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -1,60 +1,56 @@
 def notify(Map args) {
-    // Usage: 
-    // `notifySlack repositoryName: 'gh-aws', status: 'Success', additionalText: 'Some msg', sendSlackNotification: true`
+  // Usage:
+  // `slack.notify(repositoryName: 'gh-aws', status: 'Success', additionalText: 'Some msg', sendSlackNotification: true)`
+  if (!args.sendSlackNotification) { return }
 
-    if (!args.sendSlackNotification) {
-        return
-    }
+  // https://jenkins.io/doc/pipeline/examples/#slacknotify
+  label = utils.constrainLabelToSpecifications("slack-${utils.generateSlaveLabel()}")
 
-    barColor = '#36a64f'
-    if (args.status == 'Failure') {
-        barColor = '#f44242'
-    }
-    
-    // https://jenkins.io/doc/pipeline/examples/#slacknotify
-    label = utils.constrainLabelToSpecifications("slack-${utils.generateSlaveLabel()}")
-
-    lastCommit = utils.getLastCommit()
-
-    podTemplate(label: label, containers: [
-        containerTemplate(
-            name: 'gh-tools',
-            image: 'ghinfra/common-binaries:2',
-            ttyEnabled: true,
-            command: 'cat'
-        ),
-    ]) {
-        node(label) {
-            container('gh-tools') {
-                branchName = utils.getCurrentBranch()
-                title = "Jenkins Build Status"
-                fallback = "Required plain-text summary of the attachment."
-                text = "Repository: ${args.repositoryName}\nBranch: ${branchName}\n${lastCommit}\n${args.additionalText}"
-                data = """
-                { \
-                    "attachments": [ \
-                        { \
-                            "fallback": "${fallback}", \
-                            "color": "${barColor}", \
-                            "title": "${title}", \
-                            "title_link": "${env.BUILD_URL}", \
-                            "text": "${text}", \
-                            "fields": [ { "title": "Status", "value": "${args.status}", "short": false } ] \
-                        } \
-                    ] \
-                }
-                """
-                withCredentials([string(
-                    credentialsId: 'slack_enterprise_ci_bot',
-                    variable: 'slack_enterprise_ci_bot')
-                ]) {
-                    sh """
-                        curl -X POST -H 'Content-type: application/json' \
-                            --data '${data}' "${env.slack_enterprise_ci_bot}"
-                    """
-                }
-                utils.colorText(color: 'green', text: 'Slack message sent.')      
-            }
+  podTemplate(label: label, containers: [
+    containerTemplate(
+      name: 'gh-tools',
+      image: 'ghinfra/common-binaries:7',
+      ttyEnabled: true,
+      command: 'cat'
+    ),
+  ]) {
+    node(label) {
+      container('gh-tools') {
+        barColor = (args.status == 'Success') ? '#36a64f' :'#f44242' 
+        lastCommit = utils.getLastCommit()
+        branchName = utils.getCurrentBranch()
+        title = "Jenkins Build Status"
+        fallback = "Required plain-text summary of the attachment."
+        text = """
+Repository: ${args.repositoryName}
+Branch: ${branchName}
+${lastCommit}
+${args.additionalText}
+"""
+        data = """
+{ \
+  "attachments": [ \
+    { \
+      "fallback": "${fallback}", \
+      "color": "${barColor}", \
+      "title": "${title}", \
+      "title_link": "${env.BUILD_URL}", \
+      "text": "${text}", \
+      "fields": [ { "title": "Status", "value": "${args.status}", "short": false } ] \
+    } \
+  ] \
+}
+"""
+        withCredentials([string(
+          credentialsId: 'slack_enterprise_ci_bot',
+          variable: 'slack_enterprise_ci_bot')
+        ]) {
+          sh """
+curl -X POST -H 'Content-type: application/json' --data '${data}' "${env.slack_enterprise_ci_bot}"
+          """
         }
+        echo 'Slack message sent.'
+      }
     }
+  }
 }
--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,73 +1,73 @@
 def colorText(Map args) {
-    // NOTE: Color codes: http://pueblo.sourceforge.net/doc/manual/ansi_color_codes.html
-    // 
-    // Usage: 
-    // `colorText color: 'green', text: 'Some msg'`
-    
-    Map colors = [red: "41", green: "42", yellow: "43", blue: "44", magenta: "45", cyan: "46", white: "47"]
-    def selected = colors[args.color.toLowerCase()]
+  // NOTE: Color codes: http://pueblo.sourceforge.net/doc/manual/ansi_color_codes.html
+  // 
+  // Usage: 
+  // `util.colorText(color: 'green', text: 'Some msg')`
+  Map colors = [red: "41", green: "42", yellow: "43", blue: "44", magenta: "45", cyan: "46", white: "47"]
 
-    if (selected == null) {
-        selected = "47"
-    }
+  def selected = colors[args.color.toLowerCase()]
+  if (selected == null) {
+    selected = "47"
+  }
 
-    wrap([$class: 'AnsiColorBuildWrapper']) {
-        echo "\u001B[${selected}m${args.text}\u001B[0m"
-    }
+  wrap([$class: 'AnsiColorBuildWrapper']) {
+    echo "\u001B[${selected}m${args.text}\u001B[0m"
+  }
 }
 
 def getLastCommit() {
-    lastCommitScript = 'git --no-pager show -s --format="The last committer was %an/%ae, %ar%nCommit Message: %s%n"'
-    return sh(returnStdout: true, script: lastCommitScript).trim()
+  lastCommitScript = """
+git show --no-pager -s \
+  --format="The last committer was %an/%ae, %ar%nCommit Message: %s%n" \
+  | echo "Missing commit message"
+"""
+  return sh(
+    returnStdout: true, 
+    script: "${lastCommitScript}"
+  ).trim()
 }
 
 def getCurrentBranch() {
-    if (env.GIT_BRANCH) {
-        // if command is used in context of Multibranch Pipeline read value from ENV
-        return env.GIT_BRANCH
-    }
-    branchFull = scm.branches[0].name.drop(2)
-    splitBranchFull = branchFull.split('/')
-    return splitBranchFull.last()
+  // if command is used in context of Multibranch Pipeline read value from ENV
+  if (env.GIT_BRANCH) { return env.GIT_BRANCH } 
+  branchFull = scm.branches[0].name.drop(2)
+  splitBranchFull = branchFull.split('/')
+  return splitBranchFull.last()
 }
 
 def getCommitID() {
-    sh(returnStdout: true, script: "git rev-parse HEAD | tr -d '\n'")
+  sh(returnStdout: true, script: "git rev-parse HEAD | tr -d '\n'")
 }
 
 def generateSlaveLabel() {
-    if (env.BUILD_TAG) {
-        // if command is used in context of Multibranch Pipeline read value from ENV
-        return "${env.BUILD_TAG}"
-    }
-    return "slave-${UUID.randomUUID().toString()}"
+  // if command is used in context of Multibranch Pipeline read value from ENV
+  if (env.BUILD_TAG) { return "${env.BUILD_TAG}" }
+  return "slave-${UUID.randomUUID().toString()}"
 }
 
 def constrainLabelToSpecifications(String label) {
-    if(label.length() >= 63){
-        echo "Return truncated string so since it is not beyond the 63 character limit and not ending with non-alpha-numeric characters."
-        def truncatedLabel = label.substring(0, 62)
-        def length = truncatedLabel.length()
-        // Checking if trailing character is non alpha-numeric, and truncating until it is
-        while(!Character.isLetter(truncatedLabel.charAt(length - 1)) &&  !Character.isDigit(truncatedLabel.charAt(length - 1))) {
-            truncatedLabel = truncatedLabel.substring(0, length - 1)
-            length = truncatedLabel.length()
-        }
-        echo "truncatedLabel = ${truncatedLabel}"
-        return truncatedLabel
+  if (label.length() >= 63) {
+    echo "Return truncated string so since it is not beyond the 63 character limit and not ending with non-alpha-numeric characters."
+    def truncatedLabel = label.substring(0, 62)
+    def length = truncatedLabel.length()
+    // Checking if trailing character is non alpha-numeric, and truncating until it is
+    while(!Character.isLetter(truncatedLabel.charAt(length - 1)) &&  !Character.isDigit(truncatedLabel.charAt(length - 1))) {
+      truncatedLabel = truncatedLabel.substring(0, length - 1)
+      length = truncatedLabel.length()
     }
-    return label
+    echo "truncatedLabel = ${truncatedLabel}"
+    return truncatedLabel
+  }
+  return label
 }
 
 def verifySemVer(Map args) {
-    if (!args.sem_ver) {
-        return false
-    }
-    // regex checks for XX.XX.XX, where X is a decimal
-    return args.sem_ver.matches(/^([1-9]|[1-9][0-9])\.([0-9]|[1-9][0-9])\.([0-9]|[1-9][0-9])$/)
+  if (!args.sem_ver) { return false }
+  // regex checks for XX.XX.XX, where X is a decimal
+  return args.sem_ver.matches(/^([1-9]|[1-9][0-9])\.([0-9]|[1-9][0-9])\.([0-9]|[1-9][0-9])$/)
 }
 
 def checkIfEnzymeService(String serviceName) {
-    def serviceList = ["auth", "billing", "clinical", "curation", "exchange", "file", "finance", "ivd-reporting", "message", "misc", "omni-reporting", "problem-case"]
-    return serviceList.contains(serviceName)
+  def serviceList = ["auth", "billing", "clinical", "curation", "exchange", "file", "finance", "ivd-reporting", "message", "misc", "omni-reporting", "problem-case"]
+  return serviceList.contains(serviceName)
 }
