32d8afbe24bca0ac0e5fc7897164f1ab00394005
pprzekwas <pprzekwas@guardanthealth.com>
2020-03-04 15:40:16 +0100

    Update helm version for portal projects (#63)

--- vars/portal.groovy
+++ vars/portal.groovy
@@ -1,6 +1,6 @@
 // Temporary hack for helm migration to version 3
 String get_helm_bin() {
-  return (env.ENVIRONMENT == 'dev') ? 'helm3' : 'helm'
+  return (env.ENVIRONMENT in ['dev', 'sqa']) ? 'helm3' : 'helm'
 }
 
 String get_portal_version_from_dynamodb(String app_name, String project_name) {
