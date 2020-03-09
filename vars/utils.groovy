bd1a6db266af73ec28ca0454ff41491bc74eadd9
Patryk Przekwas <pkprzekwas@gmail.com>
2020-03-09 18:26:50 +0100

    Blacklisting completed and crasched pods from digest test

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -121,7 +121,7 @@ String getRecentlyDeployedPod(String pattern, String namespace) {
   sh(
     script: """
       kubectl get -n ${namespace} po --sort-by=.status.startTime | \
-        grep -v "Terminating" | \
+        grep -v -E "Terminating|Completed|Error" | \
         grep ${pattern} | \
         tac | \
         awk '{print \$1}' | \
