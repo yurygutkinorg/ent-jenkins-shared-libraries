b52868107096aa66f117a41f9972f56d1a170537
Dominik Andruszak <dandruszak@guardanthealth.com>
2020-11-05 18:19:15 +0100

    MUL-174 debug

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -141,6 +141,7 @@ def call(String mule_project, String build_tag) {
               withEnv(["RELEASE_NAME=${RELEASE_NAME}"]) {
                 withMaven(mavenSettingsFilePath: 'settings.xml') {
                   sh """
+                    mvn versions:set -DnewVersion=${env.RELEASE_NAME}
                     mvn -B clean
                   """
                 }
