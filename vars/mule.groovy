6de04fd51ae584addc62bbde5ae413868b91c8cd
gh-vadepu <74282140+gh-vadepu@users.noreply.github.com>
2021-08-03 09:02:36 +0530

    Update mule.groovy
    updated sonar non prod

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -214,7 +214,7 @@ def call(String mule_project, String build_tag) {
         steps {
           container('maven') {
             withEnv(["RELEASE_NAME=${RELEASE_NAME}"]) {
-              withSonarQubeEnv('sonar') {
+              withSonarQubeEnv('sonar-non-prod') {
                 withMaven(mavenSettingsFilePath: 'settings.xml') {
               sh 'mvn sonar:sonar'
                 }
@@ -226,7 +226,7 @@ def call(String mule_project, String build_tag) {
     
       stage("Quality Gate") {
         steps {
-          withSonarQubeEnv('sonar') {
+          withSonarQubeEnv('sonar-non-prod') {
             timeout(activity: true, time: 300, unit: 'SECONDS') {
               sleep 3
               waitForQualityGate abortPipeline: true
