54707ea47e429c3c550b77f680cb6210ba2950f5
gh-vadepu <74282140+gh-vadepu@users.noreply.github.com>
2021-08-16 23:47:48 +0530

    updated key

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -185,7 +185,8 @@ def call(String mule_project, String build_tag) {
                 credentialsId: 'MULESOFT_NEXUS_REPOSITORY', 
                 usernameVariable: 'MULE_REPOSITORY_USERNAME', 
                 passwordVariable: 'MULE_REPOSITORY_PASSWORD'
-              )
+              ),
+            string(credentialsId: "${env.ANYPOINT_KEY_SECRET_NAME}", variable: 'MULESOFT_KEY')
             ]) {
               withEnv(["RELEASE_NAME=${RELEASE_NAME}"]) {
                 withMaven(mavenSettingsFilePath: 'settings.xml') {
