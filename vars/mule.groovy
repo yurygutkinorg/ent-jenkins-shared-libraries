5aeee10116726c5686b009dd5a8b54826d142795
gh-vadepu <74282140+gh-vadepu@users.noreply.github.com>
2021-08-19 18:50:58 +0530

    updated mule token

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -169,8 +169,8 @@ def call(String mule_project, String build_tag) {
             ]) {
               withEnv(["RELEASE_NAME=${RELEASE_NAME}"]) {
                 withMaven(mavenSettingsFilePath: 'settings.xml') {
-                  def token = getConnectedAppToken()
                   script {
+                    def token = getConnectedAppToken()
                     sh """
                     set +x
                     mvn versions:set -DnewVersion=${env.RELEASE_NAME}
@@ -198,8 +198,8 @@ def call(String mule_project, String build_tag) {
             ]) {
               withEnv(["RELEASE_NAME=${RELEASE_NAME}"]) {
                 withMaven(mavenSettingsFilePath: 'settings.xml') {
-                  def token = getConnectedAppToken()
                   script {
+                    def token = getConnectedAppToken()
                     sh """
                     set +X
                     mvn -B test -DsecureKey=$MULESOFT_KEY -Dtoken=$token
