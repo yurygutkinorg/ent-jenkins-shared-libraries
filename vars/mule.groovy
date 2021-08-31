b092ee6b793c624a9195c83374d1bc2e23d5d9ce
gh-vadepu <74282140+gh-vadepu@users.noreply.github.com>
2021-08-31 05:49:42 +0530

    removed set+X

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -165,17 +165,15 @@ def call(String mule_project, String build_tag) {
                 usernameVariable: 'MULE_REPOSITORY_USERNAME', 
                 passwordVariable: 'MULE_REPOSITORY_PASSWORD'
               ),
-              
+            string(credentialsId: 'token', variable: 'token')
             ]) {
               withEnv(["RELEASE_NAME=${RELEASE_NAME}"]) {
                 withMaven(mavenSettingsFilePath: 'settings.xml') {
                   script {
                     def token = getConnectedAppToken()
                     sh """
-                    set +x
                     mvn versions:set -DnewVersion=${env.RELEASE_NAME}
                     mvn -B clean -Dtoken=$token
-                    set -x
                     """
                   }
                 }
@@ -194,16 +192,15 @@ def call(String mule_project, String build_tag) {
                 usernameVariable: 'MULE_REPOSITORY_USERNAME', 
                 passwordVariable: 'MULE_REPOSITORY_PASSWORD'
               ),
-            string(credentialsId: "${env.ANYPOINT_KEY_SECRET_NAME}", variable: 'MULESOFT_KEY')
+            string(credentialsId: "${env.ANYPOINT_KEY_SECRET_NAME}", variable: 'MULESOFT_KEY'),
+            string(credentialsId: 'token', variable: 'token')
             ]) {
               withEnv(["RELEASE_NAME=${RELEASE_NAME}"]) {
                 withMaven(mavenSettingsFilePath: 'settings.xml') {
                   script {
                     def token = getConnectedAppToken()
                     sh """
-                    set +x
                     mvn -B test -DsecureKey=$MULESOFT_KEY -Dtoken=$token
-                    set -x
                     """
                   }
                 }
