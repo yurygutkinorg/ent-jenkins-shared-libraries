3f7141da750b0108bb8a0107709f4e5498e819ca
gh-vadepu <74282140+gh-vadepu@users.noreply.github.com>
2021-09-07 13:29:43 +0530

    updated service account user/password

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -162,6 +162,11 @@ def call(String mule_project, String build_tag) {
                 credentialsId: 'MULESOFT_NEXUS_REPOSITORY', 
                 usernameVariable: 'MULE_REPOSITORY_USERNAME', 
                 passwordVariable: 'MULE_REPOSITORY_PASSWORD'
+              ),
+              usernamePassword(
+                credentialsId: 'MULESOFT_ANYPOINT_SERVICE_ACCOUNT', 
+                usernameVariable: 'ANYPOINT_USERNAME', 
+                passwordVariable: 'ANYPOINT_PASSWORD'
               )
             ]) {
               withEnv(["RELEASE_NAME=${RELEASE_NAME}"]) {
