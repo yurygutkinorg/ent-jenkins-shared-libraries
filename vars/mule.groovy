f5a3d836dc03aee6844876c076806e34421bd164
gh-vadepu <74282140+gh-vadepu@users.noreply.github.com>
2021-10-19 19:46:17 +0530

    added few cmds

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -168,6 +168,8 @@ def call(String mule_project, String build_tag) {
               withEnv(["RELEASE_NAME=${RELEASE_NAME}"]) {
                 withMaven(mavenSettingsFilePath: 'settings.xml') {
                   sh """
+                    pwd
+                    ls -lrt
                     mvn versions:set -DnewVersion=${env.RELEASE_NAME}
                     mvn -B clean
                   """
