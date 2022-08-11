4cb09c94d951c1555199e16f3e30f19356458aaa
gh-vadepu <74282140+gh-vadepu@users.noreply.github.com>
2022-08-11 20:25:29 +0000

    Update mule.groovy
    debug

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -184,6 +184,7 @@ def call(String mule_project, String build_tag) {
               withEnv(["RELEASE_NAME=${RELEASE_NAME}"]) {
                 withMaven(mavenSettingsFilePath: 'settings.xml') {
                   sh '''
+                  set +x
                     mvn versions:set -DnewVersion=${env.RELEASE_NAME}
                     mvn -B clean -Dtoken=$MULE_CONNECTED_APP_TOKEN
                   '''
